7f689a6ed49220ce193aa1c976f32356325ce7a3
Jarowin <jarowin13@gmail.com>
2022-09-20 14:01:57 +0700

    Update main.blade.php
    update favicon

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="https://i.postimg.cc/Gh840G1S/favicon.png" type="image/x-icon">
+    <link rel="icon" href="https://i.postimg.cc/D0hrDm6G/favicon.png" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Mbah Gendeng AH Pamungkas</title>
 
